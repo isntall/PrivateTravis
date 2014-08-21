a0d1e98952aae6fb0d6a28e0bc971bb4afe0acb4
Nick Schuch <nick@myschuch.com>
2014-08-21 14:17:27 +1000

    Test coverage for permutations.

--- compiler/src/Permutation.php
+++ compiler/src/Permutation.php
@@ -7,7 +7,7 @@ class Permutation {
   /**
    * The namespace of the services.
    */
-  private $namespace = "";
+  private $namespace = '';
 
   /**
    * The build steps that are run for this permuation.
@@ -15,39 +15,53 @@ class Permutation {
   private $steps = array();
 
   /**
-   * The services attached to this permuation.
+   * The links that will be attached to the main test container.
    */
-  private $services = array();
+  private $links = array();
 
   /**
-   * Adds a container service to the permutation.
+   * The language of the test that will be run.
    */
-  public addService($service) {
-    // Convert the service name to uppercase for bash variables.
-    $uppercase_service = strtoupper($service);
+  private $language = '';
 
-    // Build the service command.
-    $run = new DockerRunCommand();
-    $run->setDaemon(true);
-    $run->setRemove(true);
-    $run->setContainer($this->namespace . '/' . $service);
-    $command = $run->build();
-    $this->addStep($uppercase_service . '_ID=$(' . $command . ')');
-
-    // Build the inspect command.
-    $run = new DockerInspectCommand();
-    $run->setFormat('{{ .Name }}');
-    $run->setContainer('$' . $up_service . '_ID');
-    $run->setCommand('| cut -d "/" -f 2');
-    $command = $run->build();
-    $this->addStep($uppercase_service . '=$(' . $command . ')');
-  }
+  /**
+   * The command that gets run as part of the entry point.
+   */
+  private $cmd = '';
 
   /**
    * Build steps.
    */
-  public build($main) {
+  public function build() {
+    $namespace = $this->getNamespace();
+    $language = $this->getLanguage();
+    $links = $this->getLinks();
+    $command = $this->getCommand();
 
+    $run = new DockerRunCommand();
+    $run->setRemove(true);
+    $run->setImage($namespace . '/' . $language);
+    $run->setLinks($links);
+    $run->setCommand($command);
+
+    $commands = $this->getSteps();
+    $commands[] = $run->build();
+
+    return $commands;
+  }
+
+  /**
+   * Get namespace.
+   */
+  public function setNamespace($namespace) {
+    $this->namespace = $namespace;
+  }
+
+  /**
+   * Set namespace.
+   */
+  public function getNamespace() {
+    return $this->namespace;
   }
 
   /**
@@ -65,10 +79,87 @@ class Permutation {
   }
 
   /**
-   * Add a steo.
+   * Add a steps.
    */
   public function addStep($command) {
     $this->steps[] = $command;
+  }
+
+  /**
+   * Get namespace.
+   */
+  public function setLinks($links) {
+    $this->links = $links;
+  }
+
+  /**
+   * Set namespace.
+   */
+  public function getLinks() {
+    return $this->links;
+  }
+
+  /**
+   * Set namespace.
+   */
+  public function addLink($link) {
+    $this->links[] = $link;
+  }
+
+  /**
+   * Adds a container service to the permutation.
+   */
+  public function addService($service) {
+    // Convert the service name to uppercase for bash variables.
+    $uppercase_service = strtoupper($service);
+    $namespace = $this->getNamespace();
+
+    // Build the service command.
+    $run = new DockerRunCommand();
+    $run->setDaemon(true);
+    $run->setRemove(true);
+    $run->setImage($namespace . '/' . $service);
+    $command = $run->build();
+    $this->addStep($uppercase_service . '_ID=$(' . $command . ')');
+
+    // Build the inspect command.
+    $run = new DockerInspectCommand();
+    $run->setFormat('{{ .Name }}');
+    $run->setContainer('$' . $uppercase_service . '_ID');
+    $run->setCommand('| cut -d "/" -f 2');
+    $command = $run->build();
+    $this->addStep($uppercase_service . '=$(' . $command . ')');
+
+    // Add a link so we can access these services from the main container.
+    $this->addLink('$' . $uppercase_service . ':' . $service);
+  }
+
+  /**
+   * Get language.
+   */
+  public function setLanguage($language) {
+    $this->language = $language;
+  }
+
+  /**
+   * Set language.
+   */
+  public function getLanguage() {
+    return $this->language;
+  }
+
+  /**
+   * Gets the cmd.
+   */
+  public function setCommand($cmd) {
+    $this->cmd = $cmd;
+  }
+
+  /**
+   * Sets the cmd.
+   */
+  public function getCommand() {
+    return $this->cmd;
   }
 
 }
--- compiler/tests/PermutationTest.php
+++ compiler/tests/PermutationTest.php
@@ -4,6 +4,39 @@ namespace Compiler;
 
 class PermutationTest extends \PHPUnit_Framework_TestCase {
 
-  public function testAddService() { }
+  public function testAddService() {
+
+    $permutation = new Permutation();
+    $permutation->setNamespace('drupal');
+    $permutation->setLanguage('php5.4');
+    $permutation->setCommand('env before_script script');
+
+    // Single service.
+    $permutation->addService('mysql');
+    $steps = $permutation->getSteps();
+    $require = array(
+      'MYSQL_ID=$(docker run -d --rm drupal/mysql)',
+      'MYSQL=$(docker inspect --format "{{ .Name }}" $MYSQL_ID | cut -d "/" -f 2)',
+    );
+    $this->assertEquals($require, $steps);
+    $steps = $permutation->build();
+    $require[] = 'docker run --rm --link $MYSQL:mysql drupal/php5.4 env before_script script';
+    $this->assertEquals($require, $steps);
+
+    // More than one service.
+    $permutation->addService('postgres');
+    $steps = $permutation->getSteps();
+    $require = array(
+      'MYSQL_ID=$(docker run -d --rm drupal/mysql)',
+      'MYSQL=$(docker inspect --format "{{ .Name }}" $MYSQL_ID | cut -d "/" -f 2)',
+      'POSTGRES_ID=$(docker run -d --rm drupal/postgres)',
+      'POSTGRES=$(docker inspect --format "{{ .Name }}" $POSTGRES_ID | cut -d "/" -f 2)',
+    );
+    $this->assertEquals($require, $steps);
+    $steps = $permutation->build();
+    $require[] = 'docker run --rm --link $MYSQL:mysql --link $POSTGRES:postgres drupal/php5.4 env before_script script';
+    $this->assertEquals($require, $steps);
+
+  }
 
 }
