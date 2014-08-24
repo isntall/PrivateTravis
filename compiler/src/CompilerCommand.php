8e6628a2182785c523c2fed6a9aed85ca557f4fb
Nick Schuch <nick@myschuch.com>
2014-08-24 16:43:50 +1000

    Update args and readme.

--- compiler/src/CompilerCommand.php
+++ compiler/src/CompilerCommand.php
@@ -15,12 +15,14 @@ class CompilerCommand extends Command {
   protected function configure() {
     $this->setName('build')
          ->setDescription('Build the Docker command to run the .travis.yml file.')
-         ->addArgument('commands', InputArgument::IS_ARRAY | InputArgument::REQUIRED, 'The TravisCI commands that will be run.')
-         ->addOption('namespace', null, InputOption::VALUE_REQUIRED, 'Docker namespace to pull containers.', 1);
+         ->addArgument('commands', InputArgument::IS_ARRAY, 'The TravisCI commands that will be run.', array('env', 'before_script', 'script'))
+         ->addOption('file', null, InputOption::VALUE_REQUIRED, 'The file to load.', '.travis.yaml')
+         ->addOption('namespace', null, InputOption::VALUE_REQUIRED, 'Docker namespace to pull containers.', 'nickschuch');
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
     $namespace = $input->getOption('namespace');
+    $file = $input->getOption('file');
     $command = implode(' ', $input->getArgument('commands'));
 
     // Get the travis configuration.
